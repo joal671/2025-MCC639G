b80fd5c29cef0f0e9259ff776ce21e4690303003
joal671 <161766@unsaac.edu.pe>
2025-12-01 21:15:54 -0500

    AVL_JOSEPH 2.0

--- avl.h
+++ avl.h
@@ -1,30 +1,43 @@
 #ifndef __AVL_H__
 #define __AVL_H__
 
+#include <algorithm>
 #include "binarytree.h"
 
-template <typename Traits>
-class CAVLNode : public CBinaryTreeNode<value_type>{
+
+//        NODO AVL
+
+template <typename T>
+class CAVLNode : public CBinaryTreeNode<T>{
 public:
-  using value_type = typename Traits::T;
-  using Node       = CBinaryTreeNode<value_type>;
-protected:
-    int     m_balanceFactor = 0; // Balance factor for AVL tree
-public:
+    using Base = CBinaryTreeNode<T>;
+
+    int m_balanceFactor = 0;
+
+    CAVLNode(const T& value)
+        : Base(value), m_balanceFactor(0) {}
 };
+
+
+//               TRAITS ASCENDENTE
+
 
 template <typename _T>
 struct AVLAscTraits{
-    using  value_type = _T;
-    using  Node       = CAVLNode<T>;
-    using  CompareFn  = less<T>;
+    using value_type = _T;
+    using Node       = CAVLNode<_T>;
+    using CompareFn  = std::less<_T>;
 };
+
+
+//             TRAITS DESCENDENTE
+
 
 template <typename _T>
 struct AVLDescTraits{
-    using  value_type = _T;
-    using  Node       = CAVLNode<T>;
-    using  CompareFn  = greater<T>;
+    using value_type = _T;
+    using Node       = CAVLNode<_T>;
+    using CompareFn  = std::greater<_T>;
 };
 
 template <typename Traits>
@@ -32,27 +45,127 @@ class CAVLTree : public CBinaryTree<Traits> {
 public:
     using Base       = CBinaryTree<Traits>;
     using Node       = typename Traits::Node;
-    using value_type = typename Traits::value_type;  
+    using value_type = typename Traits::value_type;
     using CompareFn  = typename Traits::CompareFn;
-    using Container  = CAVLTree<Traits>;
-    using iterator   = binary_tree_iterator<Container>;
+    using Ref        = typename Base::Ref;
 
 protected:
-    // Additional members for AVL tree balancing can be added here
-    // TODO: modificar la insercion para que mantenga
-    //       el balance del arbo y realice las 
-    //       rotaciones necesarias
-    Node *internal_insert(value_type &elem, Ref ref,
-                          Node* pParent, Node*& rpOrigin) override
-    {
-        // TODO 1. insertar
-        // TODO 2. verificar balance
-        // TODO 3. realizar rotaciones si es necesario
-        
-        return nullptr;
+
+ 
+    //      UTILIDADES DE ARBOL AVL
+ 
+    int height(Node* node) {
+        return node ? node->m_height : 0;
     }
+
+    int calcBalance(Node* node) {
+        int hl = node->m_pLeft  ? node->m_pLeft->m_height  : 0;
+        int hr = node->m_pRight ? node->m_pRight->m_height : 0;
+        return hl - hr;
+    }
+
+    void updateNode(Node* node) {
+        int hl = node->m_pLeft  ? node->m_pLeft->m_height  : 0;
+        int hr = node->m_pRight ? node->m_pRight->m_height : 0;
+        node->m_height = std::max(hl, hr) + 1;
+        node->m_balanceFactor = hl - hr;
+    }
+
+  
+    //         ROTACIONES AVL
+  
+
+    Node* rotateRight(Node* y) {
+        Node* x  = (Node*)y->m_pLeft;
+        Node* T2 = (Node*)x->m_pRight;
+
+        x->m_pRight = y;
+        y->m_pLeft  = T2;
+
+        updateNode(y);
+        updateNode(x);
+
+        return x;
+    }
+
+    Node* rotateLeft(Node* x) {
+        Node* y  = (Node*)x->m_pRight;
+        Node* T2 = (Node*)y->m_pLeft;
+
+        y->m_pLeft  = x;
+        x->m_pRight = T2;
+
+        updateNode(x);
+        updateNode(y);
+
+        return y;
+    }
+
+
+    //    INSERCIÓN AVL MODIFICADA
+    
+
+    Node* internal_insert(value_type &elem, Ref ref,
+                          Node* pParent, Node*& node) override
+    {
+        CompareFn Compare;
+
+        // Insertar como BST normal
+        if (!node)
+        {
+            node = new Node(elem);
+            Base::m_size++;
+            return node;
+        }
+
+        if (Compare(elem, node->m_value))
+            node->m_pLeft = internal_insert(elem, ref, node, (Node*&)node->m_pLeft);
+
+        else if (Compare(node->m_value, elem))
+            node->m_pRight = internal_insert(elem, ref, node, (Node*&)node->m_pRight);
+
+        else
+            return node; // duplicado
+
+        // Actualizar altura y balance
+        updateNode(node);
+
+        int bf = node->m_balanceFactor;
+
+      
+        //   CASOS AVL
+     
+
+        // Left Left Case
+        if (bf > 1 && Compare(elem, node->m_pLeft->m_value))
+            return rotateRight(node);
+
+        // Right Right Case
+        if (bf < -1 && Compare(node->m_pRight->m_value, elem))
+            return rotateLeft(node);
+
+        // Left Right Case
+        if (bf > 1 && Compare(node->m_pLeft->m_value, elem))
+        {
+            node->m_pLeft = rotateLeft((Node*)node->m_pLeft);
+            return rotateRight(node);
+        }
+
+        // Right Left Case
+        if (bf < -1 && Compare(elem, node->m_pRight->m_value))
+        {
+            node->m_pRight = rotateRight((Node*)node->m_pRight);
+            return rotateLeft(node);
+        }
+
+        return node;
+    }
+
 public:
-    CAVLTree() : Base() {} // Empty tree
+
+    //      CONSTRUCTOR
+
+    CAVLTree() : Base() {}
 
 };
 
